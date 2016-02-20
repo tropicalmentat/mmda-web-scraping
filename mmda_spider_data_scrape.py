95980a3c34456ad1875554cf8455293bb98f0add
tropicalhobo <jose.torres@obf.ateneo.edu>
2016-02-20 23:49:55 +0800

    Fixed csv writing per row

--- mmda_spider_data_scrape.py
+++ mmda_spider_data_scrape.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import urllib
+import csv
 
 site = 'http://mmdatraffic.interaksyon.com/line-view-edsa.php'
 
@@ -28,27 +29,43 @@ for i in line_link:
     
 #crawl through links
 
-for link in links:
-    tr_line = urllib.urlopen('http://mmdatraffic.interaksyon.com'
-                             +'/'+link).read()
+with open('test.csv', 'wb') as f:
+    writer = csv.writer(f,delimiter=',')
 
-    line_soup = BeautifulSoup(tr_line, "lxml")
+    #write field names
+    field_names = ['LINE','STATUS','TIME_STAMP']
+    writer.writerow(field_names)
 
-    #print site+'/'+link
+    for link in links:
+        try:
+            tr_line = urllib.urlopen('http://mmdatraffic.interaksyon.com'
+                                     +'/'+link).read()
 
-    line_tr = line_soup.find_all("div", class_="line-row1")
-    
-    for element in line_tr:
+            line_soup = BeautifulSoup(tr_line, "lxml")
 
-        line_name = element.a.get_text().strip() #scraping line name
+            print '\n\n'+site+'/'+link+'\n\n'
 
-        #scraping traffic status
-        line_stat = element.find("div", \
-                                 class_="line-status").get_text().\
-                                 split('\n')[2].strip()
-        
-        time_stamp = element.find("div", class_="line-col" \
-                                  ).p.get_text().strip('Updated: ')
-        
-        print line_name, ': ', line_stat, ': ', time_stamp, '\n'
-        
+            line_tr = line_soup.find_all("div", class_="line-row1")
+            
+            for element in line_tr:
+
+                #scraping line name
+                line_name = element.a.get_text().strip() 
+
+                #scraping traffic status
+                line_stat = element.find("div", \
+                                         class_="line-status").get_text().\
+                                         split('\n')[2].strip()
+                
+                time_stamp = element.find("div", class_="line-col" \
+                                          ).p.get_text().strip('Updated: ')
+                
+                print line_name, ': ', line_stat, ': ', time_stamp, '\n'
+                tr_record = [line_name,line_stat,time_stamp]
+                writer.writerow([line_name,
+                                  line_stat,
+                                 time_stamp])
+
+                ##needs scraper for service roads and accident notifications
+        except:
+            UnicodeEncodeError
