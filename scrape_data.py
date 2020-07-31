e1040462bf766f36d851154a0aefc81d9d0c8958
tropicalmentat <gabtorres1011@gmail.com>
2020-07-31 17:45:46 +0800

    Added logging

--- scrape_data.py
+++ scrape_data.py
@@ -53,9 +53,28 @@ def main():
 
     now = current_timestamp()
 
+    ########################## LOGGING CONFIG ##########################
+
+    logger = logging.getLogger('mmda_traffic_history')
+    logger.setLevel(logging.INFO)
+
+    fh = logging.FileHandler('logs/mmda_traffic_history.log')
+    fh.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+
+    logger.addHandler(fh)
+
+    ####################################################################
+
     dump_name = new_csv(now)
 
-    tr_start = urllib.request.urlopen(site).read()  #TODO: handle IO error
+    try:
+        tr_start = urllib.request.urlopen(site).read()  #TODO: handle IO error
+        logger.info('Accessing {}'.format(site))
+    except Exception as e:
+        logger.error(e)
 
     # website to spider: http://mmdatraffic.interaksyon.com/line-view-edsa.php
     # code segment of interest is located from 264:271, div class "lnav"
@@ -78,8 +97,6 @@ def main():
             links.append(i.a["href"])
             #print i.a["href"]
 
-    # upload_blob()
-
     # crawl through links and write data to csv
 
     with open(dump_name,'w') as f:
@@ -94,88 +111,90 @@ def main():
             try:
                 tr_line = urllib.request.urlopen('http://mmdatraffic.interaksyon.com'
                                          + '/' + link).read().decode('utf-8')
+                logger.info('Scraping traffic status for {}'.format(link))
+            except Exception as e:
+                logger.error(e)
 
-                line_soup = BeautifulSoup(tr_line, "lxml")
+            line_soup = BeautifulSoup(tr_line, "lxml")
 
-                # print('\n\n'+site+'/'+link+'\n')
+            # print('\n\n'+site+'/'+link+'\n')
 
-                # find first line data
-                traffic_status = line_soup.find("div", class_="line-row1").contents
-                # the first child is the line name
-                # the second child is the southbound status
-                # the third child is the northbound status
-                # the timestamps of each status is embedded in the p tag
+            # find first line data
+            traffic_status = line_soup.find("div", class_="line-row1").contents
+            # the first child is the line name
+            # the second child is the southbound status
+            # the third child is the northbound status
+            # the timestamps of each status is embedded in the p tag
 
-                # remove child spaces
-                remove_space(traffic_status)
+            # remove child spaces
+            remove_space(traffic_status)
 
-                line_name = traffic_status[0].a.string
-                sb_status = remove_space(traffic_status[1].find('div',
-                                                                class_='line-status').
-                                         contents)[-1].string
+            line_name = traffic_status[0].a.string
+            sb_status = remove_space(traffic_status[1].find('div',
+                                                            class_='line-status').
+                                     contents)[-1].string
 
-                sb_timestamp = convert_timestamp(traffic_status[1].p.string)
+            sb_timestamp = convert_timestamp(traffic_status[1].p.string)
 
-                nb_status = remove_space(traffic_status[2].find('div',
-                                                                class_='line-status').
-                                         contents)[-1].string
+            nb_status = remove_space(traffic_status[2].find('div',
+                                                            class_='line-status').
+                                     contents)[-1].string
 
-                nb_timestamp = convert_timestamp(traffic_status[2].p.string)
+            nb_timestamp = convert_timestamp(traffic_status[2].p.string)
 
 
-                writer.writerow([line_name, sb_status, sb_timestamp,
-                                 nb_status, nb_timestamp])
+            writer.writerow([line_name, sb_status, sb_timestamp,
+                             nb_status, nb_timestamp])
 
-                # print("%s|%s|%s|%s|%s" % (line_name,
-                                    # sb_status, sb_timestamp,
-                                    # nb_status, nb_timestamp))
+            # print("%s|%s|%s|%s|%s" % (line_name,
+                                # sb_status, sb_timestamp,
+                                # nb_status, nb_timestamp))
 
-                # find data for each sibling of the first
-                line_sib = line_soup.find("div", class_="line-row1").next_siblings
-                for sibling in line_sib:
-                    if sibling == '\n':
-                        pass
-                    else:
-                        sibling_status = remove_space(sibling.contents)
-                        line_name = sibling_status[0].a.string
+            # find data for each sibling of the first
+            line_sib = line_soup.find("div", class_="line-row1").next_siblings
+            for sibling in line_sib:
+                if sibling == '\n':
+                    pass
+                else:
+                    sibling_status = remove_space(sibling.contents)
+                    line_name = sibling_status[0].a.string
 
-                        sb_status = remove_space(sibling_status[1].find('div',
-                                                        class_='line-status').
-                                                 contents)[-1].string
+                    sb_status = remove_space(sibling_status[1].find('div',
+                                                    class_='line-status').
+                                             contents)[-1].string
 
-                        sb_timestamp = convert_timestamp(sibling_status[1].p.string)
+                    sb_timestamp = convert_timestamp(sibling_status[1].p.string)
 
-                        nb_status = remove_space(sibling_status[2].find('div',
-                                                        class_='line-status').
-                                                 contents)[-1].string
+                    nb_status = remove_space(sibling_status[2].find('div',
+                                                    class_='line-status').
+                                             contents)[-1].string
 
-                        nb_timestamp = convert_timestamp(sibling_status[2].p.string)
+                    nb_timestamp = convert_timestamp(sibling_status[2].p.string)
 
-                        writer.writerow([line_name, sb_status, sb_timestamp,
-                                         nb_status, nb_timestamp])
+                    writer.writerow([line_name, sb_status, sb_timestamp,
+                                     nb_status, nb_timestamp])
 
-                        # print("%s|%s|%s|%s|%s" % (line_name,
-                        #                           sb_status, sb_timestamp,
-                        #                           nb_status, nb_timestamp))
+                    # print("%s|%s|%s|%s|%s" % (line_name,
+                    #                           sb_status, sb_timestamp,
+                    #                           nb_status, nb_timestamp))
 
-                # loop to inspect the html structure of line status
-                # to be used when there is a change in the over structure
-                # of the website
-                """
-                count = 1
-                for child in traffic_status.contents:
-                    if child == '\n':  # ignore the space between tags
-                        pass
-                    else:
-                        print "%d.%s" % (count, child)
-                        count += 1
-                """
+            # loop to inspect the html structure of line status
+            # to be used when there is a change in the over structure
+            # of the website
+            """
+            count = 1
+            for child in traffic_status.contents:
+                if child == '\n':  # ignore the space between tags
+                    pass
+                else:
+                    print "%d.%s" % (count, child)
+                    count += 1
+            """
 
-               # TODO: Add logging
-               # TODO: Optimize filenaming convention by removing redunant info
-               # TODO: needs scraper for service roads and accident notifications
-            except:
-                UnicodeEncodeError
+           # TODO: Add logging
+           # TODO: Optimize filenaming convention by removing redunant info
+           # TODO: needs scraper for service roads and accident notifications
+
 
     upload_blob(now,dump_name)
 
